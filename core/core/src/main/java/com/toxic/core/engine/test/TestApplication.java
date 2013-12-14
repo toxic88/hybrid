219f16995c29bd4d07ab2c3861dd2fb8e7b1f8c4
AntonSoroka <AntonSoroka@gmail.com>
2013-12-14 18:03:36 +0000

    Create application and platform object, and encapsulated implementation.

--- core/core/src/main/java/com/toxic/core/engine/test/TestApplication.java
+++ core/core/src/main/java/com/toxic/core/engine/test/TestApplication.java
@@ -1,12 +1,11 @@
-package com.toxic.core.engine;
+package com.toxic.core.engine.test;
 
 import playn.core.Font.Style;
-import playn.core.Game;
 import playn.core.PlayN;
 import playn.core.TextFormat.Alignment;
-import playn.core.util.Clock;
 import tripleplay.util.Interpolator;
 
+import com.toxic.core.engine.DataProvider;
 import com.toxic.core.engine.animation.CustomAnimation;
 import com.toxic.core.engine.base.Backgound;
 import com.toxic.core.engine.base.IApplication;
@@ -18,55 +17,18 @@ import com.toxic.core.engine.handlers.HoldHandler;
 import com.toxic.core.engine.handlers.MoveHandler;
 import com.toxic.core.engine.handlers.SelectHandler;
 import com.toxic.core.engine.resources.ITextFormat;
-import com.toxic.core.engine.util.Context;
-import com.toxic.core.engine.util.log.LogLevel;
-import com.toxic.core.engine.util.log.Logger;
 
 /**
- * <p>
- * The inner object of logic engine.
- * </p>
- * <br/>
- * 
  * @author Strelock
- * 
+ *
  */
-public final class BaseGame extends Game.Default {
-
-  static Logger log;
-
+public class TestApplication implements IApplication {
+  
   private static final float WIDTH = 50;
   private static final float HEIGHT = 50;
-  private static final int UPDATE_RATE = 30;
-  private final Clock.Source clock = new Clock.Source(UPDATE_RATE);
-  private Context context;
-  private IApplication application;
-
-  /**
-   * 
-   * <p>
-   * Default application of game engine.
-   * </p>
-   * <br/>
-   * 
-   * @param con
-   *          instance of {@link Context}
-   * @param app
-   *          instance of {@link IApplication}
-   */
-  public BaseGame(Context con, IApplication app) {
-    super(UPDATE_RATE);
-    this.context = con;
-    this.application = app;
-    DataProvider.setApplication(this);
-    DataProvider.getLogFactory().setLogLevel(LogLevel.WARN);
-    DataProvider.getLogFactory().setPrintTime(true);
-  }
 
   @Override
-  public final void init() {
-    log = DataProvider.getLogFactory().getLogger(BaseGame.class.getName());
-    this.application.init();
+  public void init() {
     final IScene main = DataProvider.createScene();
     // ANTS_TAG : provide this feature to have possibility pause game,
     // during tabs for example switched PlayN.platform.setLifeCycle()
@@ -169,30 +131,8 @@ public final class BaseGame extends Game.Default {
   }
 
   @Override
-  public final void update(int delta) {
-    this.clock.update(delta);
-    this.application.update(delta);
-  }
-
-  @Override
-  public final void paint(float alpha) {
-    this.clock.paint(alpha);
-    TimerUtility.get().update();
-    if (Scene.getCurrentScene() != null) {
-      Scene.update(this.clock);
-    }
-  }
-
-  /**
-   * <p>
-   * Context of application.
-   * </p>
-   * <br/>
-   * 
-   * @return instance of {@link Context}
-   */
-  final Context getContext() {
-    return this.context;
+  public void update(int delta) {
+    //empty block
   }
 
 }
