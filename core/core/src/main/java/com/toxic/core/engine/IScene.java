a80e2435a371eda5b13caad2053ccfb2c7b22eb1
AntonSoroka <AntonSoroka@gmail.com>
2013-12-14 08:42:44 +0000

    Encapsulated instance of Scene.

---
+++ core/core/src/main/java/com/toxic/core/engine/IScene.java
@@ -0,0 +1,25 @@
+package com.toxic.core.engine;
+
+/**
+ * <p>
+ * The basic object of activity. It allows update whole scene inner logic and
+ * switch between them.
+ * </p>
+ * <br/>
+ * @author Strelock
+ *
+ */
+public interface IScene extends IElement {
+  
+  /**
+   * <p>
+   * Activate current scene on screen.
+   * </p>
+   * <p>
+   *  <b>NOTE : if currently active some other instance of {@link IScene} it will be deactivated. </b>
+   * </p>
+   * <br/>
+   */
+  public abstract void activate();
+  
+}
