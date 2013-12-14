b06929f71c94ffc48167023717d7b74bbf24650e
AntonSoroka@gmail.com <AntonSoroka@gmail.com@520f8b58-74bb-0e4a-852c-7a09791069ae>
2013-12-14 12:19:23 +0000

    Create application object for proper consistency with core module.

--- trunk/core/flash/src/main/java/com/toxic/flash/BaseGameFlash.java
+++ trunk/core/flash/src/main/java/com/toxic/flash/BaseGameFlash.java
@@ -8,18 +8,30 @@ import playn.flash.FlashGame;
 import playn.flash.FlashPlatform;
 
 import com.toxic.core.engine.BaseGame;
+import com.toxic.core.engine.base.IApplication;
 
 /**
  * @author Strelock
- *
+ * 
  */
 public class BaseGameFlash extends FlashGame {
 
-	@Override
-    public void start() {
-	    FlashPlatform platform = FlashPlatform.register();
-	    
-	    platform.assets().setPathPrefix("prikupa/");
-	    PlayN.run(new BaseGame(new FlashContext()));
-    }
+  @Override
+  public void start() {
+    FlashPlatform platform = FlashPlatform.register();
+
+    platform.assets().setPathPrefix("prikupa/");
+    PlayN.run(new BaseGame(new FlashContext(), new IApplication() {
+
+      @Override
+      public void update(int delta) {
+        // NOOP
+      }
+
+      @Override
+      public void init() {
+        // NOOP
+      }
+    }));
+  }
 }
