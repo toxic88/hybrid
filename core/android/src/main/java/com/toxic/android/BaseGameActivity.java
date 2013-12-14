3519495bfcb24eff107eb7b95112914954ab581d
AntonSoroka <AntonSoroka@gmail.com>
2013-12-14 18:10:35 +0000

    Encapsulated application object.

--- core/android/src/main/java/com/toxic/android/BaseGameActivity.java
+++ core/android/src/main/java/com/toxic/android/BaseGameActivity.java
@@ -3,24 +3,13 @@ package com.toxic.android;
 import playn.android.GameActivity;
 import playn.core.PlayN;
 
-import com.toxic.core.engine.BaseGame;
-import com.toxic.core.engine.base.IApplication;
+import com.toxic.core.engine.BaseApp;
+import com.toxic.core.engine.test.TestApplication;
 
 public class BaseGameActivity extends GameActivity {
 
   @Override
   public void main(){
-    PlayN.run(new BaseGame(new AndroidContext(), new IApplication() {
-
-      @Override
-      public void update(int delta) {
-        // NOOP
-      }
-
-      @Override
-      public void init() {
-        // NOOP
-      }
-    }));
+    PlayN.run(new BaseApp(new AndroidContext(), new TestApplication()));
   }
 }
