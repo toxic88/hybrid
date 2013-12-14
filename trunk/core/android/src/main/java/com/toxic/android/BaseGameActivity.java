637eb8b2f401e78f2b94be3988128c5fef8945ae
AntonSoroka@gmail.com <AntonSoroka@gmail.com@520f8b58-74bb-0e4a-852c-7a09791069ae>
2013-12-14 12:17:54 +0000

    Synchronize with core project of provided application object.

--- trunk/core/android/src/main/java/com/toxic/android/BaseGameActivity.java
+++ trunk/core/android/src/main/java/com/toxic/android/BaseGameActivity.java
@@ -4,11 +4,23 @@ import playn.android.GameActivity;
 import playn.core.PlayN;
 
 import com.toxic.core.engine.BaseGame;
+import com.toxic.core.engine.base.IApplication;
 
 public class BaseGameActivity extends GameActivity {
 
   @Override
   public void main(){
-    PlayN.run(new BaseGame(new AndroidContext()));
+    PlayN.run(new BaseGame(new AndroidContext(), new IApplication() {
+
+      @Override
+      public void update(int delta) {
+        // NOOP
+      }
+
+      @Override
+      public void init() {
+        // NOOP
+      }
+    }));
   }
 }
