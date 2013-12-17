d5784a4931a2df50dd897132e59aeac1cf068535
AntonSoroka@gmail.com <AntonSoroka@gmail.com@520f8b58-74bb-0e4a-852c-7a09791069ae>
2013-12-17 00:19:45 +0000

    Commit java module

---
+++ trunk/core/java/src/main/java/com/toxic/java/PlatfromJava.java
@@ -0,0 +1,61 @@
+package com.toxic.java;
+
+import playn.core.PlayN;
+import playn.java.JavaPlatform;
+
+import com.toxic.core.engine.BaseApp;
+import com.toxic.core.engine.base.IApplication;
+import com.toxic.core.engine.base.IPlatform;
+import com.toxic.core.engine.util.Context;
+
+/**
+ * <p></p>
+ * <br/>
+ * @author Strelock
+ *
+ */
+public class PlatfromJava implements IPlatform {
+
+  private final Context context = new JavaContext();
+  protected IApplication application;
+
+  /**
+   * <p>
+   * Default hidden constructor
+   * </p>
+   * <br/>
+   * 
+   * @param app
+   *          executed instance of application.
+   */
+  public PlatfromJava(IApplication app) {
+    this.application = app;
+  }
+
+  @Override
+  public final IApplication getApp() {
+    return this.application;
+  }
+
+  @Override
+  public final Context getContext() {
+    return this.context;
+  }
+
+  /**
+   * <p>
+   * Start game launching.
+   * </p>
+   * <br/>
+   */
+  public final void start() {
+    JavaPlatform.Config config = new JavaPlatform.Config();
+    // ANTS_TAG JAVA_RESOLUTION!
+    config.height = 720;
+    config.width = 1280;
+    // config.emulateTouch= true;
+    // use config to customize the Java platform, if needed
+    JavaPlatform.register(config);
+    PlayN.run(new BaseApp(this));
+  }
+}
