6b72fbccd8df3c4b2da9bb4713356f7443d73e7f
AntonSoroka <AntonSoroka@gmail.com>
2013-12-17 06:11:27 +0000

    Added renamed PlatformHtml class.

---
+++ core/html/src/main/java/com/toxic/html/PlatformHtml.java
@@ -0,0 +1,34 @@
+package com.toxic.html;
+
+import com.toxic.core.engine.base.IApplication;
+import com.toxic.core.engine.base.IPlatform;
+import com.toxic.core.engine.util.Context;
+
+/**
+ * <p>
+ * Html version of {@link IPlatform} instance for web's platform.
+ * </p>
+ * <br/>
+ * @author Strelock
+ *
+ */
+final class PlatformHtml implements IPlatform {
+  
+  private final Context context = new HtmlContext();
+  private final IApplication application;
+  
+  PlatformHtml(IApplication app){
+    this.application = app;
+  }
+  
+  @Override
+  public Context getContext() {
+    return this.context;
+  }
+
+  @Override
+  public IApplication getApp() {
+    return this.application;
+  }
+  
+}
