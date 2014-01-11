2f6873fe1962421e7a36e9d92310d692faa708d5
AntonSoroka@gmail.com <AntonSoroka@gmail.com@520f8b58-74bb-0e4a-852c-7a09791069ae>
2014-01-11 13:57:18 +0000

    MOved archetype to root level of repository.

---
+++ archetype/src/main/resources/archetype-resources/android/src/main/java/android/__appClass__Android.java
@@ -0,0 +1,23 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.android;
+
+import com.toxic.android.PlatfromAndroid;
+import ${package}.core.${appClass};
+
+
+/**
+ * <p>Example of executing android application.</p>
+ * <br/>
+ * @author Strelock
+ *
+ */
+public class ${appClass}Android extends PlatfromAndroid {
+
+  @Override
+  public void setApplication() {
+    this.application = new ${appClass}();
+  }
+
+}
---
+++ archetype/src/main/resources/archetype-resources/java/src/main/java/java/__appClass__Java.java
@@ -0,0 +1,35 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.java;
+
+import com.toxic.core.engine.base.IApplication;
+import ${package}.core.${appClass};
+import com.toxic.java.PlatfromJava;
+
+/**
+ * <p>
+ *  Test Java application.
+ * </p>
+ * <br/>
+ * @author Strelock
+ *
+ */
+public class ${appClass}Java extends PlatfromJava {
+
+  /**
+   * <p>
+   *  Default constructor of java platform.
+   * </p> 
+   * <br/>
+   * @param app instance of application
+   */
+  public ${appClass}Java(IApplication app) {
+    super(app);
+  }
+
+  public static void main(String[] args) {
+    (new ${appClass}Java(new ${appClass}())).start();
+  }
+
+}
