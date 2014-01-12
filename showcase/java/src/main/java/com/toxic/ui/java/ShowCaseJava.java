5ecd0745f0accb67a8c85add0f5e71aa0638865d
AntonSoroka <AntonSoroka@gmail.com>
2014-01-12 04:59:14 +0000

    Share project "showcase-java" into "https://own-finance.googlecode.com/svn/trunk/showcase/java"

---
+++ showcase/java/src/main/java/com/toxic/ui/java/ShowCaseJava.java
@@ -0,0 +1,32 @@
+package com.toxic.ui.java;
+
+import com.toxic.core.engine.base.IApplication;
+import com.toxic.ui.core.ShowCase;
+import com.toxic.java.PlatfromJava;
+
+/**
+ * <p>
+ *  Test Java application.
+ * </p>
+ * <br/>
+ * @author Strelock
+ *
+ */
+public class ShowCaseJava extends PlatfromJava {
+
+  /**
+   * <p>
+   *  Default constructor of java platform.
+   * </p> 
+   * <br/>
+   * @param app instance of application
+   */
+  public ShowCaseJava(IApplication app) {
+    super(app);
+  }
+
+  public static void main(String[] args) {
+    (new ShowCaseJava(new ShowCase())).start();
+  }
+
+}
